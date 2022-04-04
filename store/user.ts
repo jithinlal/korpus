4a8870b63fc04bbe4f5a4f0e68abf6c60dab8004
jithinlalpr <jithinlal.pr@neoito.com>
2022-04-04 15:37:35 +0530

    fix: bug fix state management

--- store/user.ts
+++ store/user.ts
@@ -4,6 +4,6 @@ import { User } from "@supabase/gotrue-js";
 
 export const useStore = create((set: SetState<User | {}>) => ({
   user: supabase.auth.session()?.user,
-  setUser: (user: User) => set({ user }, true),
+  setUser: (user: User) => set(() => ({ user }), true),
   removeUser: set({ user: null }, true),
 }));
